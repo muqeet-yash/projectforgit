a5900469debcd22f37cb42103d99c1255cfcf3df
Abdul Muqeet Osama <abdulmuqeetosama99@gmail.com>
2024-05-08 05:24:22 +0000

    first commit

---
+++ webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+
+sap.ui.require(["projectforgit/test/integration/AllJourneys"
+], function () {
+	QUnit.config.autostart = false;
+	QUnit.start();
+});
---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"projectforgit/test/unit/controller/home.controller"
+], function () {
+	"use strict";
+});
